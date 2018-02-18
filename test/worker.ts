8381dd211be15e0ab66fdeb26fad6ca70981172a
Bryan Kendall <bryan@bryankendall.com>
2018-02-17 19:52:55 -0500

    adding a couple new tests and framework

--- test/worker.ts
+++ test/worker.ts
@@ -1,16 +1,51 @@
+import * as sinon from 'sinon';
 import {expect} from 'chai';
-import {PonosWorker} from '../src/worker';
+import {PonosWorker, WorkerData, WorkerFunction} from '../src/worker';
 
-describe('create', () => {
-  const fakeData = { message: '' };
+describe('PonosWorker', () => {
+  describe('create', () => {
+    it('should return a PonosWorker', () => {
+      const newWorker = PonosWorker.create(
+        0,
+        {message: ''},
+        'someQueue',
+        (data) => {});
 
-  it('should return a PonosWorker', () => {
-    const newWorker = PonosWorker.create(
-      0,
-      fakeData,
-      'someQueue',
-      (data) => {});
+      expect(newWorker).to.be.an.instanceof(PonosWorker);
+    });
+  });
 
-    expect(newWorker).to.be.an.instanceof(PonosWorker);
+  describe('run', () => {
+    it('should run our given task', () => {
+      const taskHandler = sinon.stub().resolves();
+      const worker = PonosWorker.create(
+        0,
+        {message: 'foo'},
+        'someQueue',
+        taskHandler);
+
+      const runPromise = worker.run();
+
+      return expect(runPromise).to.eventually.be.fulfilled
+        .then(() => {
+          sinon.assert.calledOnce(taskHandler);
+        });
+    });
+
+    it('should provide data to our task', () => {
+      const taskHandler = sinon.stub().resolves();
+      const worker = PonosWorker.create(
+        0,
+        {message: 'foo'},
+        'someQueue',
+        taskHandler);
+
+      const runPromise = worker.run();
+
+      return expect(runPromise).to.eventually.be.fulfilled
+        .then(() => {
+          sinon.assert.calledWithExactly(taskHandler, {message: 'foo'});
+        });
+    });
   });
 });
