6b4940e5e694398e9df4bea6922f0afdc3167fe2
Bryan Kendall <bryan.a.kendall@gmail.com>
2018-02-19 16:20:33 -0800

    adding functional tests (#5)

--- example/simple-worker.ts
+++ example/simple-worker.ts
@@ -4,20 +4,20 @@ import {Server, WorkerData, WorkerFunction} from './index';
 const basicWorker: WorkerFunction = (job: WorkerData): Promise<void> => {
   return Promise.try(() => {
     if (!job.message) {
-      throw new Error(`message is required`);
+      throw new Error('message is required');
     }
     console.log(`hello world: ${job.message}`);
   });
 };
 
 const server = new Server();
-server.setTask(`basic-queue-worker`, basicWorker);
+server.setTask('basic-queue-worker', basicWorker);
 server.start()
-  .then(() => { console.log(`server started`); })
+  .then(() => { console.log('server started'); })
   .catch((err) => { console.error(`server error: ${err}`); });
 
-process.on(`SIGINT`, () => {
+process.on('SIGINT', () => {
   server.stop()
-    .then(() => { console.log(`server stopped`); })
+    .then(() => { console.log('server stopped'); })
     .catch((err) => { console.error(`server stop error: ${err}`); });
 });
